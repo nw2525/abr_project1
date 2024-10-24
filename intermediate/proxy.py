3c395e642db2e808578bd5f8c8c3ba12d9ceb14c
Nelson Wong <nw2525@columbia.edu>
2024-10-24 13:10:05 -0400

    First chunk loads but not subsequent ones

--- intermediate/proxy.py
+++ intermediate/proxy.py
@@ -15,14 +15,15 @@ fake_ip = sys.argv[3]       # 127.0.0.1
 server_ip = sys.argv[4]     # 127.0.0.1
 server_port = '80'   # send to server
 
-file = open(log, "a")
+file = open(log, "w")
 # Set up proxy
 proxy_socket = socket(AF_INET, SOCK_STREAM)
 proxy_socket.bind((str(fake_ip), int(listen_port)))
-proxy_socket.listen(1)
+# proxy_socket.listen(1)
 
 while True:
     # connect to client(s)
+    proxy_socket.listen(1)
     client_socket, addr = proxy_socket.accept()
 
     # connect to server
@@ -33,22 +34,48 @@ while True:
 
     while connected:
         ### maybe check if still connected to client(s)/server
-        client_request = client_socket.recv(10**5)
+        print("Waiting on message from client...")
+        client_request = client_socket.recv(65536)
+        print("Request received from client")
         server_socket.send(client_request)
+        print("Request sent to server")
         chunk_start = time.time()
-        server_message = server_socket.recv(10**6)
-        end = time.time()
-        chunk_size = len(server_message)
 
-        # Process chunk for log
-        duration = end - chunk_start
+        buffer = b""
+        header_end = b"\r\n\r\n" 
+        print("Receiving header...")
+        while header_end not in buffer:
+            buffer += server_socket.recv(65536)
+
+        print("Header received. Processing...")
+        header, _, body = buffer.partition(header_end)
+        header_str = header.decode()
+        header_length = len(header)
+
+        # Arbitrary chunk name to check for errors easily
         chunk_name = "foobar"
+        for line in header_str.splitlines():
+            if line.lower().startswith("content-length"):
+                content_length = int(line.split(":")[1].strip())
+                chunk_size = header_length + content_length
 
+        print("Header processed. Buffering chunks...")
+        while len(buffer) < chunk_size:
+            chunk = server_socket.recv(65536)
+            if not chunk:
+                break
+            buffer += chunk
+
+        print("Full chunk received from server")
+        end = time.time()
+        duration = end - chunk_start
         output = str(end) + " " + str(duration) + " " + server_ip + " " + chunk_name + " " + str(chunk_size)
         print(output)
         file.write(output + "\n")
-        # Send to client
-        client_socket.sendall(server_message)
 
-client_socket.close()
-server_socket.close()
+        print("Sending chunk to client...")
+        client_socket.sendall(buffer)
+        print("Chunk sent to client")
+
+    client_socket.close()
+    server_socket.close()
