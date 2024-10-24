6740fbc249c4f041c96b9cf21b83ad504d26ad63
Nelson Wong <nw2525@columbia.edu>
2024-10-24 17:33:42 -0400

    Works for one client. Need to implement for concurrent clients

--- intermediate/proxy.py
+++ intermediate/proxy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.10
 
+import os
 from socket import *
 import requests
 import sys
@@ -15,10 +16,12 @@ fake_ip = sys.argv[3]       # 127.0.0.1
 server_ip = sys.argv[4]     # 127.0.0.1
 server_port = '80'          # send to server
 
-file = open(log, "w")
 # Set up proxy
 proxy_socket = socket(AF_INET, SOCK_STREAM)
 proxy_socket.bind((str(fake_ip), int(listen_port)))
+
+if os.path.exists(log):
+    os.remove(log)
 
 while True:
     # connect to client(s)
@@ -34,10 +37,11 @@ while True:
     header_end = b"\r\n\r\n"
     buffer = b""
     while connected:
-        ### maybe check if still connected to client(s)/server
-        print("Waiting on message from client...")
-        client_request = client_socket.recv(65536)
-        print("Request received from client")
+        file = open(log, "a")
+        client_request = 0
+        while client_request == 0:
+            client_request = client_socket.recv(8192)
+        # print("Request received from client: ", len(client_request))
 
         header, _, body = client_request.partition(header_end)
         header_str = header.decode()
@@ -45,15 +49,15 @@ while True:
         chunk_name = request_line.split(" ")[1]
 
         server_socket.send(client_request)
-        print("Request sent to server")
+        # print("Request sent to server")
         chunk_start = time.time()
 
-        print("Receiving header...")
+        # print("Receiving header...")
         while header_end not in buffer:
-            server_message = server_socket.recv(65536)
+            server_message = server_socket.recv(8192)
             buffer += server_message
 
-        print("Header received. Processing...")
+        # print("Header received. Processing...")
         header, _, body = buffer.partition(header_end)
         header_str = header.decode()
         header_length = len(header) + len(header_end)
@@ -64,34 +68,31 @@ while True:
                 content_length = int(line.split(":")[1].strip())
                 chunk_size = header_length + content_length
                 break
-        print("Header processed. Buffering chunks until:", chunk_size)
-
+        # print("Receiving server message...")
         while len(buffer) < chunk_size:
-            server_message = server_socket.recv(65536)
+            server_message = server_socket.recv(8192)
             if not server_message:
-                print("No server message")
                 break
-            print("Received chunk. Downloaded:%d/%d" %(len(buffer), chunk_size))
             buffer += server_message
         
-        cutoff = len(buffer) - chunk_size
-        print("Full chunk received from server:", len(buffer))
-        print("Sending chunk to client...")
+        cutoff = len(buffer) - chunk_size 
+        # print("Sending chunk to client...")
         if cutoff == 0:
             client_socket.sendall(buffer)
             buffer = b""
         else:
             client_socket.sendall(buffer[:-cutoff])
             buffer = buffer[-cutoff:]
-        print("Chunk sent to client")
+        # print("Chunk sent to client")
 
         end = time.time()
         duration = end - chunk_start
 
         output = str(end) + " " + str(duration) + " " + server_ip + " " + chunk_name + " " + str(chunk_size)
-        file.flush()
         print(output)
+        sys.stdout.flush()
         file.write(output + "\n")
+        file.close()
 
     client_socket.close()
     server_socket.close()
